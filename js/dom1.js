568de2826481df9cbfa9c6539740e50158c1fab3
U Sen Wan <senwan96@gmail.com>
2024-12-12 10:45:04 +0600

    getElementsByTagName

--- js/dom1.js
+++ js/dom1.js
@@ -1,3 +1,20 @@
 console.log('Hello form dom');
 console.log(document);
 console.log(document.body);
+console.log(document.getElementsByTagName('h1'));
+
+const student = {
+    name: 'John',
+    ID: 12,
+    study: function (time) {
+        console.log(time, 'It is time to study');
+    }
+}
+
+student.study('5pm');
+
+const liCollection = document.getElementsByTagName('li');
+for(const li of liCollection){
+    console.log(li);
+    console.log(li.innerText);
+}
