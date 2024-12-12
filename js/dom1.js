36e4cf630a12baa59ae6bd23cba5ea70da82279b
U Sen Wan <senwan96@gmail.com>
2024-12-12 20:43:54 +0600

    querySelector

--- js/dom1.js
+++ js/dom1.js
@@ -1,9 +1,9 @@
-console.log('Hello form dom');
+/* console.log('Hello form dom');
 console.log(document);
 console.log(document.body);
-console.log(document.getElementsByTagName('h1'));
+console.log(document.getElementsByTagName('h1')); */
 
-const student = {
+/* const student = {
     name: 'John',
     ID: 12,
     study: function (time) {
@@ -11,13 +11,21 @@ const student = {
     }
 }
 
-student.study('5pm');
+student.study('5pm'); */
 
-const liCollection = document.getElementsByTagName('li');
+/* const liCollection = document.getElementsByTagName('li');
 for(const li of liCollection){
     console.log(li);
     console.log(li.innerText);
 }
+ */
 
-const heading = document.getElementById('heading1');
-document.getElementById('dom_list');
+/* const heading = document.getElementById('heading1');
+document.getElementById('dom_list'); */
+
+const someLi = document.querySelectorAll('.dom-container li');
+console.log(someLi);
+
+
+const singleli = document.querySelector('.dom-container li').innerText;
+console.log(singleli);
